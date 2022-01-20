df2744a9ff71dc24b0c00e75767c0daaf09ba8ad
CBujeda <clarabujedamunoz@gmail.com>
2022-01-20 20:10:57 +0100

    Visor y Generador de la primera generacion

--- src/practicas/practica2/Inicio.java
+++ src/practicas/practica2/Inicio.java
@@ -2,4 +2,27 @@ package practicas.practica2;
 
 public class Inicio {
 
+	public static Boolean[][] genTable(int x, int y) {
+		Boolean[][] t = new Boolean[y][x];
+		for(int i = 0; i < t.length; i++) {
+			for(int j = 0; j < t[i].length; j++) {
+				int a = (int) Math.round(Math.random()*100);
+				if(a > 20) {
+					t[i][j] = false; 
+				}else{
+					t[i][j] = true;
+				}
+			}
+		}
+		return t;
+	}
+	
+	public static void main(String[] arg) {
+		
+		//-------------------X--Y--
+		Visor.array(genTable(10,10));
+		
+	}
+	
+	
 }
--- src/practicas/practica2/Visor.java
+++ src/practicas/practica2/Visor.java
@@ -4,7 +4,23 @@ import java.util.ArrayList;
 
 public class Visor {
 
-	public static void array (ArrayList<Integer> lista) {
+	public static void array (Boolean [][] t) {
 		
+		System.out.print("\n_");
+		for(int i = 0; i < (t[0].length); i++) {
+			System.out.print("___");
+		}
+		System.out.println();
+		for(int i = 0; i < t.length; i++) {
+			System.out.print("|" );
+			for(int j = 0; j < t[i].length;  j++) {
+				if(t[i][j] == true) {
+					System.out.print(" * ");
+				}else{
+					System.out.print("   ");
+				}
+			}
+			System.out.println();
+		}
 	}
 }

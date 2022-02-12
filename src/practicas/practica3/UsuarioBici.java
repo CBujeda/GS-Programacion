63d1885b0e8749ea0ebb875c520307f23d9e2e11
CBujeda <clarabujedamunoz@gmail.com>
2022-02-12 10:55:23 +0100

    Finalizacion de la practica (Sin comentar)

--- src/practicas/practica3/UsuarioBici.java
+++ src/practicas/practica3/UsuarioBici.java
@@ -1,8 +1,76 @@
 package practicas.practica3;
 
 public class UsuarioBici {
-
-	public UsuarioBici() {
-		
+	
+	private int usuario;
+	private int traslados;
+	private int circular;
+	private int total;
+	// Nota el total lo podemos calcular
+	//--------------------Usuario--Total traslados-Total circular-total
+	public UsuarioBici(int usuario, int traslados, int circular, int total) {
+		this.usuario = usuario;
+		this.traslados = traslados;
+		this.circular = circular;
+		this.total = total;
 	}
+
+	public void sumTraslados() {
+		this.traslados++;
+	}
+	
+	public void sumCircular() {
+		this.circular++;
+	}
+	
+	public void sumTotal() {
+		this.total++;
+	}
+	/*--------------------
+	 * GETERS Y SETERS
+	 *-------------------- 
+	 */
+	
+	public int getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(int usuario) {
+		this.usuario = usuario;
+	}
+
+	public int getTraslados() {
+		return traslados;
+	}
+
+	public void setTraslados(int traslados) {
+		this.traslados = traslados;
+	}
+
+	public int getCircular() {
+		return circular;
+	}
+
+	public void setCircular(int circular) {
+		this.circular = circular;
+	}
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	
+	/*
+	 * Set general
+	 */
+	public void setAll(int usuario, int traslados, int circular, int total) {
+		this.usuario = usuario;
+		this.traslados = traslados;
+		this.circular = circular;
+		this.total = total;
+	}
+	
 }

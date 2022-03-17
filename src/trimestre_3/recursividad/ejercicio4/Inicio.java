745ae07e2cfe9683011bae5f7955c837a29cd03c
CBujeda <clarabujedamunoz@gmail.com>
2022-03-17 19:10:08 +0100

    Actualizacion recursividad

--- src/trimestre_3/recursividad/ejercicio4/Inicio.java
+++ src/trimestre_3/recursividad/ejercicio4/Inicio.java
@@ -2,6 +2,7 @@ package trimestre_3.recursividad.ejercicio4;
 
 public class Inicio {
 	
+	public static boolean table(int[] t,int n) {return table(t,0,n,false);}
 	public static boolean table(int[] t, int i, int n, boolean temp) {
 		if(t[i] == n) {temp = true;}
 		if(i == t.length-1) {
@@ -13,7 +14,7 @@ public class Inicio {
 	
 	public static void main(String[] arg) {
 		int[] t ={1,2,3,4};
-		System.out.println(table(t,0,2,false));
+		System.out.println(table(t,2));
 		
 	}
 }

d7160e0a48861cef69fa45e67f54ff6388519571
CBujeda <clarabujedamunoz@gmail.com>
2022-03-22 09:55:22 +0100

    matricular

--- src/practicas/practica4/Interprete.java
+++ src/practicas/practica4/Interprete.java
@@ -10,6 +10,114 @@ public class Interprete {
 		ArrayList<Matricula> l = Reader.matriculas();
 		System.out.println("Numero total de matriculas: " + l.size());
 	}
+	
+	// nip = 627867 // codigos 30201,30202,30203,30204
+	public static void matricular(String nip, String codigo) {
+		ArrayList<Matricula> l = Reader.matriculas();
+		ArrayList<String[]> lAsig = Reader.asignaturas();
+		ArrayList<String[]> lAlumno = Reader.alumnos();
+		System.out.println(nip + " " + codigo);
+		String[] codigos = codigo.split(",");
+		boolean valided = true;
+		boolean esAlumno = false;
+		boolean[] esAsig = new boolean[codigos.length];
+		for(int i = 0; i < esAsig.length; i++) {
+			esAsig[i] = false;
+		}
+		boolean AsigValid = true;
+		if(!codigo.equalsIgnoreCase("false")) { // comprobacion de validez
+			for(int i = 0; i < codigos.length; i++) { 
+				try {
+					Double.parseDouble(codigos[i]);
+					if(Double.parseDouble(codigos[i]) < 0) {
+						valided = false;
+						System.out.println("Codigo invalido (no puede ser menor a 0)");
+					}
+		        }catch(NumberFormatException e) {
+		        	valided = false;
+		            System.out.println("No es numerico");
+		        }
+			}
+			try {
+		        Double.parseDouble(nip);
+		        if(Double.parseDouble(nip) < 0) {
+		        	valided = false;
+					System.out.println("Codigo invalido (no puede ser menor a 0)");
+		        }
+			}catch(NumberFormatException e) {
+	        	valided = false;
+	        	System.out.println("invalido");
+	        }
+			
+			if(valided == true) {
+				for(int i = 0; i < lAlumno.size(); i++) {
+					if(lAlumno.get(i)[0].equalsIgnoreCase(nip)) {
+						esAlumno = true;
+					}	
+				}
+				if(esAlumno == true) {
+					for(int i = 0; i < lAsig.size(); i++) {
+						for(int r = 0; r < codigos.length; r++) {
+							if(lAsig.get(i)[0].equalsIgnoreCase(codigos[r])) {
+								esAsig[r] = true;
+							}
+						}
+					}
+					for(int i = 0; i < esAsig.length; i++) {
+						if(esAsig[i] == false) {
+							AsigValid = false;
+						}
+					}
+					if(AsigValid == true) {
+						// matricular 627867 30201,30202,30203,30204
+						// matricular 627867 30201,30202,30203,30204,30206
+						
+						// revisar -------------------------------
+						System.out.println("               NIP ║  CODIGO");
+						System.out.println("══════════════════════════════");
+						boolean existe = false;
+						for(int i = 0; i < codigos.length; i++) {
+							existe = false;
+							for(int r = 0; r < l.size(); r++) {
+								if(Integer.toString(l.get(r)
+										.getNip_alumno()).equalsIgnoreCase(nip)) {
+									if(Integer.toString(l.get(r)
+											.getCod_asignatura())
+											.equalsIgnoreCase(codigos[i])) {
+										existe = true;
+									}	
+								}	
+							}
+							if(existe == false) {
+								System.out.println("Matriculando: " + nip
+															  + " " + codigos[i]);
+								l.add(new Matricula(Integer.parseInt(nip)
+												   ,Integer.parseInt(codigos[i])));
+							}else {
+								System.out.println("Matricula:    " + nip 
+										+ " " + codigos[i] + " ya existenete");
+							}
+						}
+						System.out.println("Escribiendo datos....");
+						Writter.matriculas(l); // escribiendo matriculas....
+					}else {
+						System.out.println("No se puedo ejecutar ya que "
+										 + "alguna asignatura es invalida");
+					}
+				}else {
+					System.out.println("No se puedo ejecutar ya que "
+							 + "el alumno es invalido");
+				}
+			}else {
+				System.out.println("No se puedo ejecutar ya que "
+							 	 + "los datos son erroneos");
+			}
+		}else {
+			System.out.println("No se puedo ejecutar ya que se debe "
+							 + "de constar una lista de codigos");
+		}	
+	}
+	
 	// prueba eliminar 627867 30201,30202,30203,30204
 	public static void eliminar(String nip, String codigo) {
 		ArrayList<Matricula> l = Reader.matriculas();
@@ -25,9 +133,12 @@ public class Interprete {
 					l.get(i).setNulo();
 				}else {
 					for(int r = 0; r < cadena.length; r++) {
-						if(Integer.toString(l.get(i).getCod_asignatura()).equalsIgnoreCase(cadena[r])) {
-							System.out.println("Eliminado: " + l.get(i).getNip_alumno() + "(NIP)" 
-									+ " " + l.get(i).getCod_asignatura()+ "(CODIGO)");
+						if(Integer.toString(l.get(i).getCod_asignatura())
+													.equalsIgnoreCase(cadena[r])) {
+							System.out.println("Eliminado: " + 
+													l.get(i).getNip_alumno() + "(NIP)" 
+													+ " " + l.get(i).getCod_asignatura()
+													+ "(CODIGO)");
 							l.get(i).setNulo();
 						}
 					}
@@ -145,6 +256,7 @@ public class Interprete {
 					if( command.split(" ").length == 2 ||
 						command.split(" ").length == 3) {
 						if(command.split(" ").length == 3) {
+							System.out.println("Advertencia | Codigos ingnorados");
 							if(command.split(" ")[2].equalsIgnoreCase("a")) {
 								asignaturas(true, command.split(" ")[1]); // es "A"
 							}else { asignaturas(false, command.split(" ")[1]); } // es diferente a "A"
@@ -160,15 +272,21 @@ public class Interprete {
 							}else { alumnos(false, command.split(" ")[1]); } // no contiene atributo
 						}else { esintaxis(command); }// error de sintaxis
 				}else if(command.toLowerCase().contains("eliminar")) {
-					if( command.split(" ").length == 2 ||
+					if( command.split(" ").length == 2 || // comprobacion de tamaño
 							command.split(" ").length == 3) {
-						if(command.split(" ").length == 3) { 
+						if(command.split(" ").length == 3) { // si es 3 manda datos
 							eliminar(command.split(" ")[1],
 									 command.split(" ")[2]);
-						} else { eliminar(command.split(" ")[1],"false"); }					
-					}else { esintaxis(command); }
+						} else { eliminar(command.split(" ")[1],"false"); }	//manda datos				
+					}else { esintaxis(command); } // error sintaxis
 				}else if(command.toLowerCase().contains("matricular")) {
-					System.out.println("Funciona 3");
+					if( command.split(" ").length == 2 || // si es 2 o 3
+							command.split(" ").length == 3) {
+						if(command.split(" ").length == 3) { 
+							matricular(command.split(" ")[1],
+									 command.split(" ")[2]);
+						} else { matricular(command.split(" ")[1],"false"); }					
+					}else { esintaxis(command); }					
 				}else if(command.equalsIgnoreCase("help")) {
 					Help.m1();
 				}else {

a8029463a86ec5f36357071ad0f65336ac1c97ff
CBujeda <clarabujedamunoz@gmail.com>
2022-03-23 20:44:02 +0100

    recursividad fibonachi

--- src/trimestre_3/recursividad3/ejercicio4/Inicio.java
+++ src/trimestre_3/recursividad3/ejercicio4/Inicio.java
@@ -2,20 +2,25 @@ package trimestre_3.recursividad3.ejercicio4;
 
 public class Inicio {
 	
-	public static void fibonachi(int i, int limite,int n,int temp) {
-		System.out.println(n);
-		if(i <= limite) {
-			if(i == 0) {
-				fibonachi(i+1,limite,1,temp+n);
-			}else if(i == 1){
-				fibonachi(i+1,limite,1,temp);
-			}else {
-				fibonachi(i+1,limite,n-1+temp-2,temp);
-			}
-		}	
+	public static void fibonachi(int limite) { fibonachi(0, limite);}
+	public static void fibonachi(int i, int limite) {
+		if(i < limite) {
+			System.out.print(prueba(i) + " ");
+			fibonachi(i+1,limite);
+		}
+	}
+
+	public static int prueba(int i) {
+			if (i == 0) {
+				return 0;
+			} else if(i == 1){
+				return 1;
+			} else {
+				return prueba(i - 1) + prueba(i - 2);
+			}				
 	}
 
 	public static void main(String[] arg) {
-		fibonachi(0,200,0,0);
+		fibonachi(20);
 	}
 }
